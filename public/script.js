180ca0bcab4c89a07be4be8692e9ddfd14baafec
pavlos-nikou <103177675+pavlos-nikou@users.noreply.github.com>
2023-09-17 18:04:42 +0300

    UI integration

---
+++ public/script.js
@@ -0,0 +1,40 @@
+// create elipse to help with sunrise and sunset animation
+class Elipse {
+    constructor(a,b,center){
+        this.a = a;
+        this.b = b;
+        this.center = center;
+        this.endsX = this.x(0);
+    }
+    y(x) {
+        let {a,b} = this;
+        return Math.sqrt((a*b-b*Math.pow(x,2))/a);
+    }
+    x(y) {
+        let {a,b} = this;
+        return Math.sqrt((a*b-b*Math.pow(y,2))/b);
+    }
+    step(pointsQuantity){
+        return (this.endsX*2)/pointsQuantity;
+    }
+}
+
+const tempGauge = document.querySelector(".glassUpper .temp");
+tempGauge.style.height = "6em";
+// console.log(tempGauge);
+
+
+
+const todContainer = document.querySelector(".sunriseSunsetContainer")
+console.log(todContainer.getBoundingClientRect())
+const elipse = new Elipse(200000, 20000);
+console.log(elipse.endsX);
+
+
+let points = [];
+let prevStep = -Math.floor(elipse.endsX)+5;
+for (let i = 0; i < 10; i++) {
+    points.push(prevStep);
+    prevStep = Math.floor(prevStep+elipse.step(10));
+}
+console.log(points);
